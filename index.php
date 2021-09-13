2c13faf2f54b283c7d7042ebbee758e864e4eb60
Zorro <diegoblom01@gmail.com>
2021-09-13 13:50:51 +0200

    wesbite

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="stylesheet1.css">
+</head>
+
+<body>
+    <header>
+
+        <nav>
+            <div class="banner">
+                <div class="banner_content">
+
+                    <ul>
+                        <div id="logo">
+                            <li><img src="../Website/logo.png"> </li>
+                        </div>
+                        <button>
+                            <li><a href="#">Home</a></li>
+                        </button>
+                        <button>
+                            <li><a href="#">Politiek-Juridisch</a></li>
+                        </button>
+                        <button>
+                            <li><a href="#">Sociaal-Cultureel</a></li>
+                        </button>
+                        <button>
+                            <li><a href="#">Sociaaleconomisch</a></li>
+                        </button>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+
+
+
+</body>
+
+</html>
