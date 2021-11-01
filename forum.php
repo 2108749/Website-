d98c4c95569880df4815d1bf913f7910d316cd8b
Zorro <diegoblom01@gmail.com>
2021-11-01 11:47:56 +0100

    update

--- forum.php
+++ forum.php
@@ -5,52 +5,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
     <link rel="stylesheet" href="stylesheet1.css">
+    <script src="javascript.js"></script>
+    <title>Home</title>
 </head>
+<div class="header" id="head">
+    <div class="imgg"><a href="index.php"><img class="imgg" src="logo official.png"></a></div>
+    <div class="nav">
+        <nav>
+
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="Standpunten.html">Standpunten</a></li>
+                <li><a href="agenda.html">Agenda</a></li>
+                <li><a href="login.php">Log In</a></li>
+
+                <div id="yesyes">
+                    <p>"Alles voor Nederland!"</p>
+                </div>
+            </ul>
+
+        </nav>
+    </div>
+</div>
+
+<div class="banner">
+    <h2>Welkom op onze pagina!</h2>
+    <div class="text">
+        <section>
+            <p>Onze partij streeft erna om Nederland een veiliger en ook een betere plek om te wonen. Dit willen we bereiken door oplossingen vinden voor problemen en ze oplossen bij de wortel. Wij willen een partij waarbij iedere burger iets te zeggen heeft als ze willen dat iets veranderd moet worden of iets niet. </p>
+        </section>
+    </div>
+</div>
 
 <body>
-    <header>
-
-        <nav>
-            <div class="banner">
-                <div class="banner_content">
-
-                    <ul>
-                        <div id="logo">
-                            <li><img src="../Website/logo official.png"> </li>
-                        </div>
-                        <button>
-                            <li><a href="index.php">Home</a></li>
-                        </button>
-                        <button>
-                            <li><a href="Standpunten.html">Standpunten</a></li>
-                        </button>
-                        <button>
-                            <li><a href="politicie.html">Politicie</a></li>
-                        </button>
-                        <button>
-                            <li><a href="agenda.html">agenda></a></li>
-                        </button>
-                        <button>
-                            <li><a href="forum.php">Forum</a></li>
-                        </button>
-                        <div id="loginbutton">
-                            <button>
-                                <li><a href="login.php">Login</a></li>
-                            </button>
-                        </div>
-                        <button>
-                            <li><a href="blog.html">Blog</a></li>
-                        </button>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
-    <main>
-
-    </main>
 
 
 
